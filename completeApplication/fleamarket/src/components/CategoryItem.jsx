a6e3a36491c51d7c49cef7fca4a17f8ac3cf0a56
Susanne Fischer <susanne-fischer98@web.de>
2025-08-22 14:23:40 +0200

    React - adding category Item

--- completeApplication/fleamarket/src/components/CategoryItem.jsx
+++ completeApplication/fleamarket/src/components/CategoryItem.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+function CategoryItem() {
+    const [categories, setCategories] = useState([]);
 
-function CategoryItem(){
-
-    const [data, setData] = useState([]);
-    fetchCategories();
-    async function fetchCategories(){
-        try{
-             const response = await fetch('http://localhost:8080/Demo/categories');
-             const existingCategories = await response.json();
-             setData(existingCategories);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('http://localhost:8080/Demo/categories');
+                const existingCategories = await response.json();
+                setCategories(existingCategories);
+            } catch (error) {
+                console.error("Kategorie-Fehler:", error);
+                alert("Fehler beim Laden der Kategorien");
+            }
         }
-        catch(error){
-            alert("Fehler bei der Datenbank-Verbindung:"+ console.error());
-        }
-       
-    }
+
+        fetchData();
+    }, []);
+
     return (
         <>
-            <div>{data[0]}</div>
-            <div>{data[1]}</div>
-            <div>{data[2]}</div>
-            <div>{data[3]}</div>
-            <div>{data[4]}</div>
+            {categories.map((category, index) => (
+                <div key={index}>{category}</div>
+            ))}
         </>
     );
 }
 
-export default CategoryItem
+export default CategoryItem;
