7b8918934bd495fec8fbe656a3c3b72b20de2781
Susanne Fischer <susanne.fischer@springer-berlin.de>
2025-09-28 05:39:59 +0200

    React - editing Product-Info-Page (CSS)

--- completeApplication/fleamarket/src/components/ProductItemInfo.jsx
+++ completeApplication/fleamarket/src/components/ProductItemInfo.jsx
@@ -6,29 +6,29 @@ import { useNavigate } from "react-router-dom";
 function ProductItemInfo({thisProduct, onItemClick}) {
   return (
     <div>
-         <div className="popUp-container">
-        <div className="popUp-content">
-        <div className="exitDiv"  onClick={() => onItemClick(false)} >
-          <ExitIcon />
-        </div>
-        <h1>{thisProduct.title}</h1>
-        <p>{thisProduct.description}</p>
-        <p><b>{thisProduct.price} €</b></p>
-        <p>{thisProduct.condition}</p>
-        <p>{thisProduct.available}</p>
-        <p><b>{thisProduct.category} </b></p>
-        <p>{thisProduct.id}</p>
-        <img src={thisProduct.images[0]} alt={thisProduct.name}></img>
-        <p><b>{thisProduct.location}</b></p>
-        <p>{thisProduct.name}</p>
-        <p>{thisProduct.userId}</p>
-        <p><b>{thisProduct.category} </b></p>
-        <p>{thisProduct.id}</p>
-      </div>
-    </div>
+        <div className="popUp-container">
+            <div className="exitDiv"  onClick={() => onItemClick(false)} >
+                <ExitIcon />
+            </div>
+            <div className="popUp-content">
+                <h1>{thisProduct.title}</h1>
+                <p>{thisProduct.description}</p>
+                <div className="productInfos"><b>price </b>{thisProduct.price} €</div>
+                <div className="productInfos"><b>condition </b>{thisProduct.condition}</div>
+                <div className="productInfos"><b>location: </b>{thisProduct.location}</div>
 
-
-        
+                <p>{thisProduct.available}</p>
+                <img src={thisProduct.images[0]} alt={thisProduct.name}></img>
+                <p>{thisProduct.name}</p>
+                <p>{thisProduct.userId}</p>
+                <button className="productBTN">add to cart</button>
+                <button className="productBTN">contact seller</button>
+                <div >
+                  <p className="footer">{thisProduct.category} </p>
+                  <p className="footer">{thisProduct.id}</p>
+                </div>
+            </div>            
+        </div>     
     </div> 
   );
 }
